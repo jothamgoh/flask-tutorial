94e7d2506c8a431ec3f7c169b4a499bae847326c
jothamgoh <35646888+jothamgoh@users.noreply.github.com>
2020-07-19 12:08:15 +0800

    updated

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,6 +10,7 @@ from flask.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth') # the blueprint needs to know where it’s defined, so __name__ is passed as the second argument. The url_prefix will be prepended to all the URLs associated with the blueprint.
 
+
 @bp.route('/register', methods=('GET', 'POST')) # @bp.route associates the URL /register with the register view function. When Flask receives a request to /auth/register, it will call the register view and use the return value as the response.
 def register():
     if request.method == 'POST': # user submitted the form, request.method will be 'POST'. start validating the input.
@@ -38,4 +39,36 @@ def register():
     return render_template('auth/register.html') # When the user initially navigates to auth/register, or there was a validation error, an HTML page with the registration form should be shown. render_template() will render a template containing the HTML
 
 
-@
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone() # The user is queried first and stored in a variable for later use.
+
+        if user is None:
+            error = 'Incorrect username'
+        elif not check_password_hash(user['password'], password): # hashes the submitted password in the same way as the stored hash and securely compares them
+            error = 'Incorrect password'
+
+        if error is None:
+            # session is a dict that stores data across requests. When validation succeeds, the user’s id is stored in a new session.
+            # The data is stored in a cookie that is sent to the browser, and the browser then sends it back with subsequent requests. 
+            # Flask securely signs the data so that it can’t be tampered with.
+            session.clear() 
+            session['user_id'] = user['id'] # key 'user_id' in session dict. 
+            return redirect(url_for('index')) 
+
+        flash(error)
+
+        return render_template('auth/login.html')
+
+
+bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
